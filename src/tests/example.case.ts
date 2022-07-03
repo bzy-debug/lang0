7893bae380f0959f7f0e81c74a7920daa20dab8e
Xie Yuheng <xyheme@gmail.com>
2022-07-03 16:11:25 +0800

    up

--- src/tests/example.case.ts
+++
@@ -1,8 +0,0 @@
-import { TestCase } from "@xieyuheng/test-case"
-
-export default class extends TestCase {
-  async "Example test."() {
-    this.assertEquals(1 + 1, 2)
-    this.assertNotEquals(1 + 1, 3)
-  }
-}

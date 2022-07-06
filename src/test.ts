c0f827a9ea991a401f5b52ef9c734dbd751cb034
Xie Yuheng <xyheme@gmail.com>
2022-07-06 22:15:15 +0800

    - test `Exp.evaluate`

--- src/test.ts
+++ src/test.ts
@@ -6,8 +6,15 @@ import { Env, Value, Var, Fn, Ap } from "./index"
   // (lambda (f) (lambda (x) x))
 
   const env = new Env()
-  const exp = new Var("x")
-  // console.log(exp.evaluate(env), value === exp.evaluate(env))
+  const exp = new Ap(
+    // (lambda (t) (lambda (f) t))
+    new Fn("t", new Fn("f", new Var("t"))),
+    // (lambda (x) x)
+    new Fn("x", new Var("x"))
+  )
+  const value = exp.evaluate(env)
+
+  console.dir(value, { depth: null })
 }
 
 // ((lambda (t) (lambda (t) t)) (lambda (x) x))

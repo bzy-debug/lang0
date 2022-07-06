e13e3ee0b4844ad0559cbcd2bbba81427a85800e
bzy <bzy-sustech@foxmail.com>
2022-07-06 22:46:35 +0800

    more tests

--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,4 @@
-import { Env, Value, Var, Fn, Ap } from "./index"
+import { Ap, Env, Fn, Var } from "./index"
 
 {
   // ((lambda (t) (lambda (f) t)) (lambda (x) x))
@@ -7,9 +7,7 @@ import { Env, Value, Var, Fn, Ap } from "./index"
 
   const env = new Env()
   const exp = new Ap(
-    // (lambda (t) (lambda (f) t))
     new Fn("t", new Fn("f", new Var("t"))),
-    // (lambda (x) x)
     new Fn("x", new Var("x"))
   )
   const value = exp.evaluate(env)
@@ -17,12 +15,34 @@ import { Env, Value, Var, Fn, Ap } from "./index"
   console.dir(value, { depth: null })
 }
 
-// ((lambda (t) (lambda (t) t)) (lambda (x) x))
-// =>
-// (lambda (t) t)
+{
+  // ((lambda (t) (lambda (t) t)) (lambda (x) x))
+  // =>
+  // (lambda (t) t)
 
-// ((lambda (t) (t (lambda (t) t))) (lambda (x) x))
-// =>
-// ((lambda (x) x) (lambda (t) t))
-// =>
-// (lambda (t) t)
+  const env = new Env()
+  const exp = new Ap(
+    new Ap(new Fn("t", new Fn("t", new Var("t"))), new Fn("x", new Var("x"))),
+    new Fn("z", new Var("t"))
+  )
+  const value = exp.evaluate(env)
+
+  console.dir(value, { depth: null })
+}
+
+{
+  // ((lambda (t) (t (lambda (t) t))) (lambda (x) x))
+  // =>
+  // ((lambda (x) x) (lambda (t) t))
+  // =>
+  // (lambda (t) t)
+
+  const env = new Env()
+  const exp = new Ap(
+    new Fn("t", new Ap(new Var("t"), new Fn("t", new Var("t")))),
+    new Fn("x", new Var("x"))
+  )
+  const value = exp.evaluate(env)
+
+  console.dir(value, { depth: null })
+}
